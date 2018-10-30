221c239050acd10d46f989fa0f2e85ace861393d
Jani Patokallio <jpatokal@iki.fi>
2018-10-30 17:17:45 +1100

    More Docker config

---
+++ sinatra-startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+gem install sinatra
+if [ "$RACK_ENV" == "production" ]; 
+then 
+  bundle install --without development test
+  ruby $MAIN_APP_FILE -p 80
+else
+  bundle install
+  if [ "$RACK_ENV" == "test" ]; 
+  then
+    rspec
+  else
+    gem install sinatra-contrib
+    ruby $MAIN_APP_FILE -p 80 -o '0.0.0.0'
+  fi
+fi
+
